39e12875d084fd08aca74227643ada23d36723ff
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-27 14:49:38 +0700

    Add files via upload

---
+++ cocoaPodsDemo/cocoaPodsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cocoaPodsDemo
+//
+//  Created by Zulfikar Abdul Rahman Suwardi on 27/10/22.
+//
+
+import UIKit
+import CLTypingLabel
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var clTypingLabel: CLTypingLabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    clTypingLabel.text = "CL Typing Label"
+  }
+
+
+}
+
+// Add cocoa Pods
+
